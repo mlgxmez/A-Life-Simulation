b639e0ea5e2f168c9f1296e983977e4fddea94c0
mlgxmez <mlgxmez@gmail.com>
2020-07-19 13:24:10 +0200

    Add activity log to Person class

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "Person.h"
 
 int Person::id = 0;
@@ -5,4 +6,14 @@ int Person::id = 0;
 int Person::getId()
 {
     return _id;
+}
+
+void Person::writeToLog(int i)
+{
+    _activityLog[i] += 1;
+}
+
+void Person::printLog()
+{
+    printf("Person ID: %d, Eat: %2d, Sleep: %2d, Commute: %2d\n", this->getId(), _activityLog[0], _activityLog[1], _activityLog[2]);
 }
--- src/Person.h
+++ src/Person.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <array>
 class Person
 {
     
@@ -8,9 +8,13 @@ class Person
         Person(): _id(Person::id)
         {
             ++Person::id;
+            _activityLog={0,0,0};
         }
         int getId();
+        void writeToLog(int i);
+        void printLog();
 
     private:
         const int _id;
+        std::array<int,3> _activityLog;
 };
